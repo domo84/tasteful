9725e1c24ec6f3effc2620408d0e46c7b2dfe638
Magnus Hellman <magnus.hellman@gmail.com>
2018-02-20 09:25:49 +0100

    UPDATE Access Control headers

--- src/Response.php
+++ src/Response.php
@@ -2,6 +2,13 @@
 
 namespace Sunnyvale\REST;
 
+/**
+ * @note Access-Control-Allow-Headers always include, no need to add them:
+ * - Accept
+ * - Accept-Language
+ * - Content-Language
+ * - Content-Type
+ */
 abstract class Response
 {
     public $code = null;
@@ -9,6 +16,6 @@ abstract class Response
     public $headers = array(
         "Access-Control-Allow-Origin: *",
         "Access-Control-Allow-Methods: DELETE, GET, HEAD, OPTIONS, POST, PUT",
-        "Access-Control-Allow-Headers: Authorization, Content-Type"
+        "Access-Control-Allow-Headers: Authorization"
     );
 }
