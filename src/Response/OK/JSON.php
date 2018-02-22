c0f740ed2adab8a691a4a5b67691fbddb998b07c
Magnus Hellman <magnus.hellman@gmail.com>
2018-02-22 11:51:18 +0100

    Sometimes the elevator does not go all the way up

--- src/Response/OK/JSON.php
+++ src/Response/OK/JSON.php
@@ -13,11 +13,12 @@ class JSON extends OK
 
     public function __construct($body)
     {
+        $this->decoded = $body; # Saved for ld+json purposes
+
         $body = $this->isJSON($body) ? $body : $this->encode($body);
         $body .= "\n\r";
 
         $this->body = $body;
-        $this->decoded = $body; # Saved for ld+json purposes
 
         $this->headers[] = "Content-Type: application/ld+json; charset=utf-8";
         $this->headers[] = "Content-Length: " . strlen($this->body);
